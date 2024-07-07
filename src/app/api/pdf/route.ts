d2646a561c2e6b1e59c2408676ece3bec1e88fc8
Hiago Guedes <hiago@contasimples.com>
2024-07-07 04:31:25 -0300

    fix: remove tipagem do get do router

--- src/app/api/pdf/route.ts
+++ src/app/api/pdf/route.ts
@@ -1,10 +1,9 @@
 import { Client } from '@notionhq/client';
-import { NextApiRequest } from 'next';
 import { NextResponse } from 'next/server';
 
 const notion = new Client({ auth: process.env.NOTION_INTEGRATION_TOKEN });
 
-export async function GET(request: NextApiRequest) {
+export async function GET() {
     
     const blocks = await notion.blocks.retrieve({ block_id: process.env.NOTION_CURRICULUM_PDF! });
     
