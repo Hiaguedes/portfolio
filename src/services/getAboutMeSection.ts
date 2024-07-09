402bce971101c1f8988c34288336d59c6e1a31e4
Hiago Guedes <hiago@contasimples.com>
2024-07-09 06:13:56 -0300

    fix: add error on request

--- src/services/getAboutMeSection.ts
+++ src/services/getAboutMeSection.ts
@@ -3,6 +3,10 @@ const getAboutMeSection = async () => {
     const api = `${process.env.URL ?? 'http://localhost:3000'}/api/about-me`;
     const response = await fetch(api, { cache: 'no-cache' });
 
+    if(!response.ok){
+        throw new Error('Failed to fetch data');
+    }
+
     return response.json()
 }
 
