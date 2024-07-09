ccfb67d126f94d793de65cfd79eadac22f08169d
Hiago Guedes <hiago@contasimples.com>
2024-07-09 05:47:30 -0300

    fix: await json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
 export default async function Home() {
   const data = await getRepos();
-  const aboutMe = await getAboutMeSection();
+  const aboutMe = await (await getAboutMeSection()).json();
 
   return (
     <>
--- src/services/getAboutMeSection.ts
+++ src/services/getAboutMeSection.ts
@@ -3,9 +3,8 @@ const getAboutMeSection = async () => {
 
     const api = `${process.env.URL ?? 'http://localhost:3000'}/api/about-me`;
     const response = await fetch(api, { signal });
-    const aboutMe = await response.json();
 
-    return aboutMe
+    return response
 }
 
 export default getAboutMeSection;
